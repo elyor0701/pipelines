366756e4098fc5add91e33d0884a14903869031f
elur <elyorbekuralov@gmail.com>
2022-04-17 13:11:15 +0500

    SingleHash changed, works faster

--- signer.go
+++ signer.go
@@ -15,7 +15,7 @@ func ExecutePipeline(jobs ...job) {
 	wg := &sync.WaitGroup{}
 	for _, work := range jobs {
 		wg.Add(1)
-		out := make(chan interface{}, 1)
+		out := make(chan interface{}, 100)
 		go func(in, out chan interface{}, work job, wg *sync.WaitGroup) {
 			defer wg.Done()
 			work(in, out)
@@ -28,17 +28,40 @@ func ExecutePipeline(jobs ...job) {
 
 func SingleHash(in, out chan interface{}) {
 	start := time.Now()
+	wg := &sync.WaitGroup{}
+	mu := &sync.Mutex{}
 	for data := range in {
-		dataCrc32Ch := SingleCrc32Helper(strconv.Itoa(data.(int)))
-		dataMd5 := DataSignerMd5(strconv.Itoa(data.(int)))
-		dataCrc32Md5 := DataSignerCrc32(dataMd5)
-		dataCrc32 := <-dataCrc32Ch
-		result := dataCrc32 + "~" + dataCrc32Md5
-		out <- result
+		wg.Add(1)
+		go func(data string) { // check without chan parametr
+			defer wg.Done()
+			dataCrc32Ch := SingleCrc32Helper(data)
+			mu.Lock()
+			dataMd5 := DataSignerMd5(data)
+			mu.Unlock()
+			dataCrc32Md5 := DataSignerCrc32(dataMd5)
+			dataCrc32 := <-dataCrc32Ch
+			result := dataCrc32 + "~" + dataCrc32Md5
+			out <- result
+		}(strconv.Itoa(data.(int)))
 	}
+	wg.Wait()
 	end := time.Since(start)
-	fmt.Println("Single", end)
+	fmt.Println("SingleH", end)
 }
+
+// func SingleHash(in, out chan interface{}) {
+// 	start := time.Now()
+// 	for data := range in {
+// 		dataCrc32Ch := SingleCrc32Helper(strconv.Itoa(data.(int)))
+// 		dataMd5 := DataSignerMd5(strconv.Itoa(data.(int)))
+// 		dataCrc32Md5 := DataSignerCrc32(dataMd5)
+// 		dataCrc32 := <-dataCrc32Ch
+// 		result := dataCrc32 + "~" + dataCrc32Md5
+// 		out <- result
+// 	}
+// 	end := time.Since(start)
+// 	fmt.Println("Single", end)
+// }
 
 func SingleCrc32Helper(data string) chan string {
 	result := make(chan string, 1)
@@ -67,7 +90,6 @@ type ThWorker struct {
 }
 
 func MultiHash(in, out chan interface{}) {
-	start := time.Now()
 	inWorker := make(chan ThWorker, 5)
 	outWorker := make(chan ThWorker, 5)
 	for i := 0; i < 6; i++ {
@@ -92,8 +114,6 @@ func MultiHash(in, out chan interface{}) {
 		out <- result
 	}
 	close(inWorker)
-	end := time.Since(start)
-	fmt.Println("Multihash", end)
 }
 
 func CombineResults(in, out chan interface{}) {
